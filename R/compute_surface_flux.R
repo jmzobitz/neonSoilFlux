0df483957760e2e8598959186ef7ce8435aa25d0
jmzobitz <zobitz@augsburg.edu>
2022-06-23 10:46:30 -0500

    Updated computing surface flux with correct directionality.

--- R/compute_surface_flux.R
+++ R/compute_surface_flux.R
@@ -15,6 +15,8 @@
 # changelog and author contributions / copyrights
 #   John Zobitz (2021-07-21)
 #     original creation
+#   John Zobitz (2022-06-21)
+#     revision to account for correct directionality when computing fluxes
 
 compute_surface_flux <- function(input_data) {
 
@@ -22,23 +24,24 @@ compute_surface_flux <- function(input_data) {
   input_data_rev <- input_data %>%
     arrange(desc(zOffset)) %>%
     slice(1:3) %>%
-    mutate(zOffset = abs(zOffset))
+    mutate(zOffset = abs(zOffset))  # Make these positive, just to avoid problems down the road.
 
   # Calculate diffusivity of the first two levels as harmonic mean
   diffusive_mean <- c( (sum(input_data_rev$diffus[1:2]^(-1))/2)^(-1), (sum(input_data_rev$diffus[2:3]^(-1))/2)^(-1) )
 
-  # Calculate diffusive flux between CO2 measurement levels (µmol m-2 s-1)
-  diffusive_flux <- c((input_data_rev$co2mol[2] - input_data_rev$co2mol[1]) / (input_data_rev$zOffset[1] - input_data_rev$zOffset[2] ) * diffusive_mean[1],
-                      (input_data_rev$co2mol[3] - input_data_rev$co2mol[2]) / (input_data_rev$zOffset[2] - input_data_rev$zOffset[3] ) * diffusive_mean[2] )
+  # Calculate diffusive flux between CO2 measurement levels (µmol m-2 s-1).
+  # This is Fick's law, so Flux = -Da *(gradient)
+  # Note: Because the depth is a positive number, graphs of CO2 vs depth will be increasing, so a negative diffusive flux means that the flux moves DOWN (negative)  ** sigh Calculus **
+  diffusive_flux <- -diff(input_data_rev$co2mol)/diff(input_data_rev$zOffset) * diffusive_mean
 
   # Determine the average depth (m) between measurement levels so that this can be assigned to the flux measurement
-  diffusive_flux_depth <- c ( (((input_data_rev$zOffset[2]-input_data_rev$zOffset[1])/2)+input_data_rev$zOffset[1]), (((input_data_rev$zOffset[3]-input_data_rev$zOffset[2])/2)+input_data_rev$zOffset[2]) )
+  diffusive_flux_depth <- diff(input_data_rev$zOffset)/2 + input_data_rev$zOffset[1:2]
 
 
   # Now compute the fluxes, adding on the harmonic mean, fluxes, and depths
   flux_depth <- input_data_rev %>%
     slice(1:2) %>%
-    mutate(diff_hm = diffusive_mean,
+    mutate(diff_hm = diffusive_mean, # hm = harmonic mean
            flux = diffusive_flux,
            diff_depth = diffusive_flux_depth)
 
@@ -48,9 +51,15 @@ compute_surface_flux <- function(input_data) {
 
 
   # Calculate soil CO2 diffusive flux at the soil surface (i.e., flux to the atmosphere) by linear extrapolation of the fluxes to the soil surface (µmol m-2 s-1).
-  surface_flux <- flux_depth$flux[1] - (((flux_depth$flux[1] - flux_depth$flux[2]) / (abs(flux_depth$diff_depth[1]) - abs(flux_depth$diff_depth[2]))) * abs(flux_depth$diff_depth[1]))
+  surface_flux <- flux_depth$flux[1] - (((flux_depth$flux[1] - flux_depth$flux[2]) / (flux_depth$diff_depth[1] - flux_depth$diff_depth[2])) * flux_depth$diff_depth[1])
 
-  return(surface_flux)
+  # Now when we have a positive surface flux, that means CO2 is moving down to the soil. (or UP the depth vs concentration plot) Flux gradient method will be violated, let's just return NA
+
+  if(surface_flux > 0) {return(NA)}
+  else {
+    return(-surface_flux) # Make the surface flux positive for consistency * sigh * minus signs
+  }
+
 
 
 }
--- process-files/data-process.R
+++ process-files/data-process.R
@@ -2,10 +2,10 @@ library(tidyverse)
 library(NEONSoils)
 
 # --> YOU ADD: Define the name of the site:
-sites <- c("SRER","SJER","WREF")  # Add in the 4 letter codes for the other sites WREF
+sites <- c("WREF")  # Add in the 4 letter codes for the other sites WREF
 
 # --> Create the dates vector You can shorten these to a single month if you want.
-dates <- c("2021-05","2021-06")
+dates <- c("2021-01","2021-02")
 
 # Combine these two together:
 site_date_combine <- expand_grid(sites,dates)
@@ -16,7 +16,7 @@ for(i in seq_along(site_date_combine)) {
   # Name current month (you will need to adjust this on your computer)
   curr_site <- site_date_combine$sites[[i]]
   curr_date <- site_date_combine$dates[[i]]
-  acquire_name <- "data/curr-flux.Rda"
+  acquire_name <- paste0('data/',curr_site,"-NEON-flux-",curr_date,".Rda")
   env_name <- paste0('data/',curr_site,"-env-meas-",curr_date,".Rda")
   flux_name <- paste0('data/',curr_site,"-out-flux-",curr_date,".Rda")
   print(env_name)
@@ -27,9 +27,9 @@ for(i in seq_along(site_date_combine)) {
                         file_name = acquire_name,
                         env_file_name = env_name)
 
-    out_fluxes <- compute_neon_flux("data/curr-flux.Rda")
+   # out_fluxes <- compute_neon_flux("data/curr-flux.Rda")
 
-    save(out_fluxes,file=flux_name)
+  #  save(out_fluxes,file=flux_name)
 
 }
 
